927f51ba422150b7ad0f62da3be49decae5dc3a4
Sergey <sergeygarnov1991@gmail.com>
2018-06-08 23:20:19 +0300

    table paging (not working)

--- src/components/SwapiPeopleTable.jsx
+++ src/components/SwapiPeopleTable.jsx
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
+import TableFooter from '@material-ui/core/TableFooter';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import LinearProgress from "@material-ui/core/LinearProgress";
+import TablePagination from '@material-ui/core/TablePagination';
+import TablePaginationActionsWrapped from './TablePaginationActionsWrapped'
+
 
 const styles = theme => ({
   root: {
@@ -17,28 +19,53 @@ const styles = theme => ({
   },
   table: {
     minWidth: 700,
-  },
+  }
 });
 
+const peopleUrl = "https://swapi.co/api/people/"
 
 export default class SwapiPeopleTable extends Component {
                  constructor(props) {
                    super(props);
 
                    this.state = {
+                     count: 0,
                       people: [],
-                      isBusy: false
+                      isBusy: false,
+                      prevPeople: '',
+                      nextPeople: '',
+                      page: 1,
+                      rowsPerPage: 10
                      };
                  }
                  componentDidMount() {
-                   this.setState({isBusy: true})
-                   fetch("https://swapi.co/api/people/")
+                  this.updatePeople()
+                 }
+                 updatePeople(){
+                  this.setState({isBusy: true})
+              
+                   fetch(peopleUrl + '?page=' + this.state.page)
                      .then(response => response.json())
-                     .then(data => {
-                       this.setState({ people: data.results });
-                       this.setState({ isBusy: false })
+                     .then(data => {                       
+                       this.setState({
+                          people: data.results,
+                          isBusy: false,
+                          prevPeople: data.previous,
+                          nextPeople: data.next,
+                          count: data.count
+                       })
                      });
                  }
+
+                 handleChangePage = (event, page) => {
+                  this.setState({ page: page });
+                  this.updatePeople()
+                };
+              
+                handleChangeRowsPerPage = event => {
+                  this.setState({ rowsPerPage: event.target.value });
+                };
+
                  render() {
                    const { people } = this.state;
                    return <Paper className={styles.root}>
@@ -82,7 +109,21 @@ export default class SwapiPeopleTable extends Component {
                                </TableRow>;
                            })}
                          </TableBody>
+                         <TableFooter>
+                          <TableRow>
+                            <TablePagination
+                              colSpan={3}
+                              count={this.state.count}
+                              rowsPerPage={this.state.rowsPerPage}
+                              page={this.state.page}
+                              onChangePage={this.handleChangePage}
+                              ActionsComponent={TablePaginationActionsWrapped}
+                              onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                            />
+                          </TableRow>
+                        </TableFooter>
                        </Table>
+                       
                        {this.state.isBusy ? <LinearProgress /> : null}
                      </Paper>;
                  }
---
+++ src/components/TablePaginationActionsWrapped.jsx
@@ -0,0 +1,86 @@
+import React, {Component} from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
+import PropTypes from 'prop-types';
+
+const actionsStyles = theme => ({
+    root: {
+      flexShrink: 0,
+      color: theme.palette.text.secondary,
+      marginLeft: theme.spacing.unit * 2.5,
+    },
+  });
+
+  export default class TablePaginationActions extends Component {
+    // propTypes = {
+    //   count: PropTypes.number.isRequired,
+    //   onChangePage: PropTypes.func.isRequired,
+    //   page: PropTypes.number.isRequired,
+    //   rowsPerPage: PropTypes.number.isRequired
+    // }
+    constructor(props) {
+      super(props);
+      this.state = {
+        page: props.page
+        };
+    }
+    handleFirstPageButtonClick = event => {
+      this.props.onChangePage(event, 1);
+    };
+  
+    handleBackButtonClick = event => {
+      this.props.onChangePage(event, this.state.page - 1);
+    };
+  
+    handleNextButtonClick = event => {
+      this.props.onChangePage(event, this.state.page + 1);
+    };
+  
+    handleLastPageButtonClick = event => {
+      this.props.onChangePage(
+        event,
+        Math.max(0, Math.ceil(this.state.count / this.state.rowsPerPage) - 1),
+      );
+    };
+  
+    render() {
+      const { count, page, rowsPerPage } = this.props;
+      return (
+        <div className={actionsStyles.root}>
+          <IconButton
+            onClick={this.handleFirstPageButtonClick}
+            disabled={page === 1}
+            aria-label="First Page"
+          >
+          <FirstPageIcon />
+          </IconButton>
+          <IconButton
+            onClick={this.handleBackButtonClick}
+            disabled={page === 1}
+            aria-label="Previous Page"
+          >
+          <KeyboardArrowLeft />
+          </IconButton>
+          <IconButton
+            onClick={this.handleNextButtonClick}
+            disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+            aria-label="Next Page"
+          >
+          <KeyboardArrowRight />
+          </IconButton>
+          <IconButton
+            onClick={this.handleLastPageButtonClick}
+            disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+            aria-label="Last Page"
+          >
+          <LastPageIcon />
+          </IconButton>
+        </div>
+      );
+    }
+  }
+
+  
