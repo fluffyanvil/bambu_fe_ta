174d0edd9fc84adb257ccad0cdbf37c96ba7197c
sergey.garnov <sergeygarnov1991@gmail.com>
2018-06-09 16:10:12 +0300

    load nested

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import SwapiPeopleTable from './components/SwapiPeopleTable.jsx'
-import { Grid } from '@material-ui/core';
 
 class App extends Component {
   render() {
